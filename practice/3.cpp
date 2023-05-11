57941044402b2ae2a9c19694212d627b64fccf5d
Utso Roy <85325828+utso185-h@users.noreply.github.com>
2023-05-11 13:19:58 +0600

    Add files via upload

---
+++ practice/3.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a;
+    cin>>n;
+    if(n%5==0){
+        a=n/5;
+
+    }
+    else
+        a=(n/5)+1;
+
+cout<<a;
+    return 0;
+}
+
